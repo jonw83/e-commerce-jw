9491a40baf06d8916f9c63f2db00bc8c27f037b3
Jonathan Walters <jonfwalters@gmail.com>
2019-06-19 14:18:34 +0000

    type correction

--- accounts/views.py
+++ accounts/views.py
@@ -40,7 +40,7 @@ def login(request):
         login_form = UserLoginForm()
     return render(request, "login.html", {"login_form": login_form})
 
-def registration(request):
+def register(request):
     """Render the registration page"""
     if request.user.is_authenticated:
         return redirect(reverse('index'))
